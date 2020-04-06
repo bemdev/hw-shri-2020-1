1766e8c67f50596ff9016ad8312d7cb69a73ef0f
bemdev <mediaforge1@yandex.ru>
2020-04-06 07:52:58 +0400

    SHRIHOMEWORKIII-657/done

--- config/config.js
+++ config/config.js
@@ -8,108 +8,111 @@ const { NODE_ENV = 'development' } = process.env;
 const IS_DEVELOPMENT = NODE_ENV === 'development';
 
 function commonConfig() {
-	return {
-		port: 3000,
-		publicPath: '/assets/',
-	};
+    return {
+        port: 3000,
+        publicPath: '/assets/',
+    };
 }
 
 function createTarget({
-	// Target name
-	target,
+    // Target name
+    target,
 }) {
-	let { publicPath } = commonConfig();
+    let { publicPath } = commonConfig();
 
-	/**
-	 * Root of project
-	 */
-	let root = join(__dirname, '../app');
+    /**
+     * Root of project
+     */
+    let root = join(__dirname, '../app');
 
-	/**
-	 * Path for compiled assets
-	 */
-	let dist = join(root, '../public', target);
+    /**
+     * Path for compiled assets
+     */
+    let dist = join(root, '../public', target);
 
-	/**
-	 * Source directory
-	 */
-	let src = join(root, 'src');
+    /**
+     * Source directory
+     */
+    let src = join(root, 'src');
 
-	/**
-	 * Name of output bundles
-	 */
-	let name = IS_DEVELOPMENT ? '[name].js' : '[hash:16].js';
+    /**
+     * Name of output bundles
+     */
+    let name = IS_DEVELOPMENT ? '[name].js' : '[hash:16].js';
 
-	let IS_SERVER = target === 'server';
-	let IS_CLIENT = target === 'client';
+    let IS_SERVER = target === 'server';
+    let IS_CLIENT = target === 'client';
 
-	return {
-		root,
-		src,
-		dist,
+    return {
+        root,
+        src,
+        dist,
 
-		isDevelopment: IS_DEVELOPMENT,
+        isDevelopment: IS_DEVELOPMENT,
 
-		webpack: {
-			name: target,
-			target: 'node',
-			entry: join(src, target + '.js'),
-			devtool: IS_DEVELOPMENT ? 'cheap-module-eval-source-map' : false,
-			mode: NODE_ENV,
-			// watch: IS_DEVELOPMENT,
+        webpack: {
+            name: target,
+            target: 'node',
+            entry: join(src, target + '.js'),
+            devtool: IS_DEVELOPMENT ? 'cheap-module-eval-source-map' : false,
+            mode: NODE_ENV,
+            // watch: IS_DEVELOPMENT,
 
-			output: {
-				path: dist,
-				filename: name,
-				chunkFilename: name,
-				publicPath,
-			},
+            output: {
+                path: dist,
+                filename: name,
+                chunkFilename: name,
+                publicPath,
+            },
 
-			stats: {
-				entrypoints: true,
-			},
+            stats: {
+                entrypoints: true,
+            },
 
-			resolve: {
-				modules: ['node_modules', 'src/components'],
-				extensions: ['.js', '.jsx', '.json'],
-			},
+            resolve: {
+                modules: ['node_modules', 'src/components'],
+                extensions: ['.js', '.jsx', '.json'],
+            },
 
-			module: {
-				rules: [
-					{
-						test: /\.js?|.jsx?/,
-						exclude: /node_modules/,
-						use: {
-							loader: 'babel-loader',
-							options: {
-								presets: ['@babel/preset-env', '@babel/preset-react'],
-								plugins: ['@babel/plugin-transform-runtime'],
-							},
-						},
-					},
-				],
-			},
+            module: {
+                rules: [
+                    {
+                        test: /\.js?|.jsx?/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [
+                                    '@babel/preset-env',
+                                    '@babel/preset-react',
+                                ],
+                                plugins: ['@babel/plugin-transform-runtime'],
+                            },
+                        },
+                    },
+                ],
+            },
 
-			plugins: [
-				//if we need define how check side uncomment this
-				// new webpack.DefinePlugin({
-				// 	IS_SERVER: JSON.stringify(IS_SERVER),
-				// 	IS_CLIENT: JSON.stringify(IS_CLIENT),
-				// 	'typeof window': JSON.stringify(IS_CLIENT ? 'object' : 'undefined'),
-				// 	'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-				// }),
+            plugins: [
+                //if we need define how check side uncomment this
+                // new webpack.DefinePlugin({
+                // 	IS_SERVER: JSON.stringify(IS_SERVER),
+                // 	IS_CLIENT: JSON.stringify(IS_CLIENT),
+                // 	'typeof window': JSON.stringify(IS_CLIENT ? 'object' : 'undefined'),
+                // 	'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+                // }),
 
-				new webpack.NoEmitOnErrorsPlugin(),
+                new webpack.NoEmitOnErrorsPlugin(),
 
-				...(IS_DEVELOPMENT ? [] : [new UglifyJSPlugin()]),
+                // ...(IS_DEVELOPMENT ? [] : [new UglifyJSPlugin()]),
 
-				new WebpackLoggerPlugin(),
-			],
-		},
-	};
+                new WebpackLoggerPlugin(),
+            ],
+        },
+    };
 }
 
 module.exports = {
-	commonConfig,
-	createTarget,
+    commonConfig,
+    createTarget,
 };
