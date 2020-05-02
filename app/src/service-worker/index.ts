1319e298e1ca332fc95b4d422ac37faf268c59ee
bemdev <mediaforge1@yandex.ru>
2020-05-03 00:44:11 +0400

    add worker

---
+++ app/src/service-worker/index.ts
@@ -0,0 +1,30 @@
+export const CACHE_NAME = 'shri-ci-v1';
+
+import { update, fromCacheOrNetwork } from './helpers';
+
+self.addEventListener('install', function(event:any) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event: any) {
+    //позволяет клиентской странице, загруженной в той же области видимости, обходиться без перезагрузки для использования сервис-воркером.
+    event.waitUntil(clients.claim());
+
+    // Remove unwanted caches
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (CACHE_NAME !== cache) {
+                        return caches.delete(cache);
+                    }
+                }),
+            );
+        }),
+    );
+});
+
+self.addEventListener('fetch', function(event:any) {
+    event.respondWith(fromCacheOrNetwork(event.request));
+    event.waitUntil(update(event.request));
+});
